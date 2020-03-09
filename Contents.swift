cfb2d2f60fc2a815247880074fc74e9607acb362
juliana <j.sierrac@uniandes.edu.co>
2020-03-09 19:06:16 +0100

    Project

---
+++ Contents.swift
@@ -0,0 +1,19 @@
+ import UIKit
+
+var numeros = 1...100
+
+for numero in numeros{
+    if(numero % 5==0){
+        print("\(numero) Bingo")
+    }
+    if(numero % 2==0){
+        print("\(numero) par")
+    }
+    if(numero % 2==1){
+        print("\(numero) impar")
+    }
+    if(numero>=30 && numero<=40) {
+       print("\(numero) Viva Swift!!!")
+    }
+}
+
